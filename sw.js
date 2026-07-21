4f8c2e28cd5ab63bcda61045e48f8dd046a342db
小金 <xiaojin@xiaojindeMacBook-Pro.local>
2026-07-22 07:04:40 +0800

    feat: add course catalog and improve touch answers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='guanshan-v3';
+const CACHE='guanshan-v6';
 const ASSETS=['./','./index.html','./styles.css','./library.js','./app.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
