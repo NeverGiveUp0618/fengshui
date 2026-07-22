0972c0e610bf7d10097ec624c52b18551d289a61
小金 <xiaojin@xiaojindeMacBook-Pro.local>
2026-07-22 10:10:50 +0800

    Automate course completion and audit date logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='guanshan-v11';
+const CACHE='guanshan-v12-audit';
 const ASSETS=['./','./index.html','./styles.css','./library.js','./image-bank.js','./app.js','./manifest.json','./assets/photos/mingtang-levels.jpg','./assets/photos/five-stars-pass.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
